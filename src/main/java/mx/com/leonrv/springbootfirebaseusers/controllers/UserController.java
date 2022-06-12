1090aea1e7758f4c6966c8b67c0a91cd21c99b45
LeonRivera <leonriv2@gmail.com>
2022-06-12 14:31:37 -0500

    12062022 | LeonRivera | Create update method

--- src/main/java/mx/com/leonrv/springbootfirebaseusers/controllers/UserController.java
+++ src/main/java/mx/com/leonrv/springbootfirebaseusers/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,6 +76,11 @@ public class UserController {
 
     @PostMapping
     public Mono<User> save(@RequestBody User user){
+        return userService.save(user);
+    }
+    
+    @PutMapping
+    public Mono<User> update(@RequestBody User user){
         return userService.save(user);
     }
 
