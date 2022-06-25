ac801389b1563aeafb1c4d4866ac4fa00f703cfc
LeonRivera <leonriv2@gmail.com>
2022-06-25 13:15:54 -0500

    25062022 | LeonRivera | CRUD Api Rest Operations for Provider entity

---
+++ src/main/java/mx/com/leonrv/springbootfirebaseusers/services/ProveedorService.java
@@ -0,0 +1,40 @@
+package mx.com.leonrv.springbootfirebaseusers.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mx.com.leonrv.springbootfirebaseusers.models.Proveedor;
+import mx.com.leonrv.springbootfirebaseusers.repositories.IProveedorRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProveedorService implements IProveedorService{
+    
+
+    @Autowired(required = false)
+    IProveedorRepository proveedorRepository;
+
+    @Override
+    public Flux<Proveedor> findAll() {
+        return proveedorRepository.findAll();
+    }
+
+    @Override
+    public Mono<Proveedor> save(Proveedor proveedor) {
+        return proveedorRepository.save(proveedor);
+    }
+
+    @Override
+    public Mono<Void> delete(Proveedor proveedor) {
+        return proveedorRepository.delete(proveedor);
+    }
+
+    @Override
+    public Mono<Proveedor> getByRfc(String rfc) {
+        return proveedorRepository.findById(rfc);
+    }
+
+
+
+}
