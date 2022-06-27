ef74a9456b97e512c479c335bdb56105faf1038a
LeonRivera <leonriv2@gmail.com>
2022-06-27 14:16:48 -0500

    27062022 | LeonRivera | Proveedores CRUD Completed :b

--- src/main/java/mx/com/leonrv/springbootfirebaseusers/controllers/ProveedorController.java
+++ src/main/java/mx/com/leonrv/springbootfirebaseusers/controllers/ProveedorController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/proveedores")
+@CrossOrigin(origins = "*")
 public class ProveedorController {
     
     @Autowired
